3c3831f60fe64f7a829ed35058f14240d48d0804
aji70 <realjaiboi70@gmail.com>
2026-04-22 12:47:43 +0100

    update

--- backend-old/config/database.js
+++
@@ -1,7 +0,0 @@
-import knex from "knex";
-import knexfile from "../knexfile.js";
-
-const env = process.env.NODE_ENV || "development";
-const config = knexfile[env] || knexfile.development;
-
-export default knex(config);
